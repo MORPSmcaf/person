46dbb9eae4659d6a2a6cfabe8dd276f40f678242
MCAF <akhthar.bawazeer@mcaf.nb.ca>
2024-06-07 12:34:07 -0700

    Changed Class name to CustomClient

--- person/models/client.py
+++ person/models/client.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 
-class Person_inherit(models.Model):
+class CustomClient(models.Model):
     _name = 'custom.client'
     _description = 'Model of a client'
     _inherit = 'abstract.person'
