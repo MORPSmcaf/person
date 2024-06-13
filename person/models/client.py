b7c3274ee7c6d424e71ff4dedccb8f3164e08a41
zam786 <zainab.masood@mcaf.nb.ca>
2024-06-13 09:39:20 -0700

    [FIX] Changes done

--- person/models/client.py
+++ person/models/client.py
@@ -1,9 +1,16 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class CustomClient(models.Model):
     _name = 'custom.client'
     _description = 'Model of a client'
     _inherit = 'abstract.person'
 
-    photo = fields.Char(string="photo")
+    photo = fields.Binary(string="Photo")
+    postalcode = fields.Char(string="Postal Code")
+
+    @api.constrains('postalcode')
+    def _check_postalcode(self):
+        for record in self:
+            if record.postalcode and len(record.postalcode) != 6:
+                raise ValidationError("Postal Code must be exactly 6 characters long.")
